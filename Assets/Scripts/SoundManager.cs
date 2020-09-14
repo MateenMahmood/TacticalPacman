2634014b83ce22c07c0aebdd148fa8355030da5e
MateenMahmood <13896093@student.uts.edu.au>
2020-09-14 22:19:53 +1000

    Test2

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -8,7 +8,6 @@ public class SoundManager : MonoBehaviour
     [SerializeField] private AudioSource startJingle = null;
     [SerializeField] private AudioSource mainMusic = null;
 
-    //Test
     // Start is called before the first frame update
     void Start() {
         startJingle.Play();
