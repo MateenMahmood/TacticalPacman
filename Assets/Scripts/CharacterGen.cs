f95d9fa52ebbaf304af5dc63fe1b234b25e04fb4
MateenMahmood <13896093@student.uts.edu.au>
2020-10-12 22:24:41 +1100

    Added World Space Canvas over the Ghosts

--- Assets/Scripts/CharacterGen.cs
+++ Assets/Scripts/CharacterGen.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CharacterGen : MonoBehaviour
 {
+    [SerializeField] Camera mainCamera = null;
     [SerializeField] GameObject Pacman = null;
     [SerializeField] GameObject Inky = null;
     [SerializeField] GameObject Blinky = null;
@@ -11,15 +12,14 @@ public class CharacterGen : MonoBehaviour
     [SerializeField] GameObject Clyde = null;
     // Start is called before the first frame update
     void Start() {
-        Instantiate(Pacman, new Vector3(0.32f, -0.32f, 0), Quaternion.identity);
-        Instantiate(Inky, new Vector3(3.84f, -4.48f, 0), Quaternion.identity);
-        Instantiate(Blinky, new Vector3(4.16f, -4.48f, 0), Quaternion.identity);
-        Instantiate(Pinky, new Vector3(4.48f, -4.48f, 0), Quaternion.identity);
-        Instantiate(Clyde, new Vector3(4.8f, -4.48f, 0), Quaternion.identity);
-    }
-
-    // Update is called once per frame
-    void Update() {
-        
+        GameObject pacman = Instantiate(Pacman, new Vector3(0.32f, -0.32f, 0), Quaternion.identity);
+        GameObject inky = Instantiate(Inky, new Vector3(3.84f, -4.48f, 0), Quaternion.identity);
+        GameObject blinky = Instantiate(Blinky, new Vector3(4.16f, -4.48f, 0), Quaternion.identity);
+        GameObject pinky = Instantiate(Pinky, new Vector3(4.48f, -4.48f, 0), Quaternion.identity);
+        GameObject clyde = Instantiate(Clyde, new Vector3(4.8f, -4.48f, 0), Quaternion.identity);
+        inky.GetComponentInChildren<Canvas>().worldCamera = mainCamera;
+        blinky.GetComponentInChildren<Canvas>().worldCamera = mainCamera;
+        pinky.GetComponentInChildren<Canvas>().worldCamera = mainCamera;
+        clyde.GetComponentInChildren<Canvas>().worldCamera = mainCamera;
     }
 }
