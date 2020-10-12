b2b45347d67bb69172158c5211888b6277e05d96
MateenMahmood <13896093@student.uts.edu.au>
2020-10-12 15:45:21 +1100

    Changed Manager Setup

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,23 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
     // All Serialize Fields should be set to as null to stop warning
-    [SerializeField] private AudioSource startJingle = null;
+    [SerializeField] private AudioSource startMusic = null;
     [SerializeField] private AudioSource mainMusic = null;
+    int sceneIndex;
 
-    // Start is called before the first frame update
     void Start() {
-        startJingle.Play();
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
-    // Update is called once per frame
     void Update() {
-        
-        if (!startJingle.isPlaying && !mainMusic.isPlaying) {
-            mainMusic.Play();
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (sceneIndex == 0) {
+
+            if (!startMusic.loop) {
+                startMusic.loop = true;
+            }
+
+            if (!startMusic.isPlaying) {
+                startMusic.Play();
+            }
+        }
+
+        if (sceneIndex == 1) {
+            
+            if (startMusic.loop) {
+                startMusic.loop = false;
+            }
+
+            if (!startMusic.isPlaying && !mainMusic.isPlaying) {
+                mainMusic.Play();
+            }
         }
     }
 }
