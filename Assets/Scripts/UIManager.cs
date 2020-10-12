e756ceb6a441cad556844a591f97a9f785ef69e5
MateenMahmood <13896093@student.uts.edu.au>
2020-10-12 20:53:12 +1100

    Added HUD Layout with Dummy Data

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,8 +5,21 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
-{
+{   
     public void LoadLevelOne() {
+        SceneManager.sceneLoaded += OnSceneLoaded;
         SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame() {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        if (scene.buildIndex == 1) {
+            Button quitButton = GameObject.FindWithTag("QuitButton").GetComponent<Button>();
+
+            quitButton.onClick.AddListener(QuitGame);
+        }
     }
 }
